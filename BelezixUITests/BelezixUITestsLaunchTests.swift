05c3187ab94e3a0e83b41582bd7d889f30df51ad
gustavo.aguiar <gustavo.aguiar@redspark.io>
2024-12-16 09:50:29 -0300

    first commit

---
+++ BelezixUITests/BelezixUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BelezixUITestsLaunchTests.swift
+//  BelezixUITests
+//
+//  Created by Gustavo Miranda de Aguiar on 12/12/24.
+//
+
+import XCTest
+
+final class BelezixUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
